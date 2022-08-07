e5930a378cd216f805a41b6eb17074157e7e3f0c
shawn <shawnzhu@ichuk.com>
2022-08-07 18:13:04 +0800

    建立飞船

--- test.py
+++ test.py
@@ -1,7 +1,35 @@
 # -*- coding:utf-8 -*-
 # authority by Nutter
+# exam 12-4
 
-import random
+import sys
+import pygame
 
-num = random.random()
-print(num)
+
+def run_game():
+    # 主窗口
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("按键监控")
+
+    # 显示单元
+    show_screen = pygame.display.set_mode((200, 100))
+    show_rect = show_screen.get_rect()
+    show_rect.x = 0
+    show_rect.y = 0
+
+    while True:
+        screen.fill((230, 0, 0))
+        screen.blit(show_screen, show_rect)
+        pygame.display.flip()
+
+
+def show_key_event():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            return pygame.event
+
+
+run_game()
