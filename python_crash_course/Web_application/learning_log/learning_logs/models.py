216b64d9091cbb81ea2ee6e75493549bca82e962
“Rainrelaxme” <rainrelaxme@foxmail.com>
2023-04-26 20:30:14 +0800

    learn django

--- python_crash_course/Web_application/learning_log/learning_logs/models.py
+++ python_crash_course/Web_application/learning_log/learning_logs/models.py
@@ -6,8 +6,26 @@ from django.db import models
 class Topic(models.Model):
     """用户学习的主题"""
     text = models.CharField(max_length=200)
-    date_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)    # 创建时间为当前时间
 
     def __str__(self):
         """返回模型的字符串表示"""
         return self.text
+
+
+class Entry(models.Model):
+    """学到的某个主题的具体指示。"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)  # 关联外键，级联删除
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta类用于管理模型的额外信息。"""
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """返回模型的字符串表示"""
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return self.text
